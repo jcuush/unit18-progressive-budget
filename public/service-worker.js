c664e95287c12f3ebbbf21eeb89ec754484d3c95
jcuush <jrcushing@sbcglobal.net>
2020-11-16 22:43:12 -0600

    edited service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,37 +1,39 @@
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/index.js',
-    'icons/icon-192x192.png',
-    'icons/icon-512x512.png',
-    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+    "/", 
+    "/index.html", 
+    "/index.js", 
+    "/styles.css", 
+    "/icons/icon-192x192.png", 
+    "/icons/icon-512x512.png"
   ];
 
-  const PRECACHE = 'precache-v1';
-  const RUNTIME = 'runtime';
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
 
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
-  });
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches
+      .open(STATIC_CACHE)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
 
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
     event.waitUntil(
       caches
         .keys()
-        .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+        .then(cacheNames => {
+          // return array of cache names that are old to delete
+          return cacheNames.filter(
+            cacheName => !currentCaches.includes(cacheName)
+          );
         })
-        .then((cachesToDelete) => {
+        .then(cachesToDelete => {
           return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
+            cachesToDelete.map(cacheToDelete => {
               return caches.delete(cacheToDelete);
             })
           );
@@ -40,22 +42,34 @@ const FILES_TO_CACHE = [
     );
   });
 
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
+  self.addEventListener("fetch", event => {
+    if (event.request.url.includes("/api/")) {
       event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
-          }
-
-          return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
-            });
-          });
+        caches.open(RUNTIME_CACHE).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              cache.put(event.request, response.clone());
+              return response;
+            })
+            .catch(() => caches.match(event.request));
         })
       );
+      return;
     }
+  
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+  
+        return caches.open(RUNTIME_CACHE).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
   });
